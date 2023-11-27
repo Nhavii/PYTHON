ea2118fc694b00c037d5d800d658632621bd4e88
Nhavii <151572359+Nhavii@users.noreply.github.com>
2023-11-27 14:54:37 +0100

    PIEDRA PAPEL TIJERAS

--- EJERCICIOS DE PRACTICA/PIEDRA PAPEL TIJERAS.py
+++ EJERCICIOS DE PRACTICA/PIEDRA PAPEL TIJERAS.py
@@ -2,59 +2,32 @@ import random
 opciones=["Piedra","Papel","Tijeras"]
 
 jugador= input("Elige entre Piedra, Papel o Tijeras: ")
-jugador=jugador.capitalize
+jugador=str(jugador.capitalize())
 
 pc=random.choice(opciones)
 
-if jugador=="Piedra":
-    if pc==jugador:
-        print(f"Pringao empatasteis, la pc saco {pc}")
-    elif pc=="Piedra" and jugador=="Papel":
-        print(f"Ganaste, la pc saco {pc}")
-    elif pc=="Piedra" and jugador=="Tijeras":
-        print(f"Perdiste, la pc saco {pc}")
-    elif pc=="Papel" and jugador=="Piedra":
-        print(f"Perdiste, la pc saco {pc}")
-    elif pc=="Papel" and jugador=="Tijeras":
-        print(f"Ganaste, la pc saco {pc}")
-    elif pc=="Tijeras" and jugador=="Papel":
-        print(f"Perdiste, la pc saco {pc}")
-    elif pc=="Tijeras" and jugador=="Piera":
-        print(f"Ganaste, la pc saco {pc}")
-if jugador=="Papel":
-    if pc==jugador:
-        print(f"Pringao empatasteis, la pc saco {pc}")
-    elif pc=="Piedra" and jugador=="Papel":
-        print(f"Ganaste, la pc saco {pc}")
-    elif pc=="Piedra" and jugador=="Tijeras":
-        print(f"Perdiste, la pc saco {pc}")
-    elif pc=="Papel" and jugador=="Piedra":
-        print(f"Perdiste, la pc saco {pc}")
-    elif pc=="Papel" and jugador=="Tijeras":
-        print(f"Ganaste, la pc saco {pc}")
-    elif pc=="Tijeras" and jugador=="Papel":
-        print(f"Perdiste, la pc saco {pc}")
-    elif pc=="Tijeras" and jugador=="Piera":
-        print(f"Ganaste, la pc saco {pc}")
-if jugador=="Tijeras":
-    if pc==jugador:
-        print(f"Pringao empatasteis, la pc saco {pc}")
-    elif pc=="Piedra" and jugador=="Papel":
-        print(f"Ganaste, la pc saco {pc}")
-    elif pc=="Piedra" and jugador=="Tijeras":
-        print(f"Perdiste, la pc saco {pc}")
-    elif pc=="Papel" and jugador=="Piedra":
-        print(f"Perdiste, la pc saco {pc}")
-    elif pc=="Papel" and jugador=="Tijeras":
-        print(f"Ganaste, la pc saco {pc}")
-    elif pc=="Tijeras" and jugador=="Papel":
-        print(f"Perdiste, la pc saco {pc}")
-    elif pc=="Tijeras" and jugador=="Piera":
-        print(f"Ganaste, la pc saco {pc}")
-else:
-    print("Ponga una opcion valida.")
-    exit()
 
+if jugador==pc:
+        print(f"Payaso habeis empatado, la pc saco {pc}")
+elif jugador=="Piedra":
+    if pc=="Papel":
+                print(f"Perdiste, la pc saco {pc} y tu {jugador}")
+    else:
+                print(f"Ganaste, la pc saco {pc} y tu {jugador}")
+elif jugador=="Papel": 
+    if pc=="Tijeras":
+                print(f"Perdiste, la pc saco {pc} y tu {jugador}")
+    else:
+                print(f"Ganaste, la pc saco {pc} y tu {jugador}")
+elif jugador=="Tijeras": 
+        if pc=="Piedra":
+                print(f"Perdiste, la pc saco {pc} y tu {jugador}")
+        else:
+                print(f"Ganaste, la pc saco {pc} y tu {jugador}")
+else:
+    print("Error- Opcion no valida.")
+
+                
 
 
 
